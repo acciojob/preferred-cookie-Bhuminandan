6d9bb6646d7f37b2a0e3d33faba8d55550ee9655
Bhuminandan <134225833+Bhuminandan@users.noreply.github.com>
2023-08-21 16:08:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
-let body = document.getElementsByTagName("body")[0];
-let form = document.getElementsByTagName("form")[0];
-let submitBtn = document.querySelector("#sumbit-btn");
+function setCookie(name, value, days) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+  document.cookie = `${name}=${encodeURIComponent(value)};expires=${expires.toUTCString()}`;
+}
 
-submitBtn.addEventListener("click", (element) => {
-  element.preventDefault();
-  let fontSize = document.querySelector("#fontsize");
-  let colorInput = document.querySelector("#fontcolor");
+function getCookie(name) {
+  const cookieName = `${name}=`;
+  const cookieArray = document.cookie.split(';');
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return decodeURIComponent(cookie.substring(cookieName.length));
+    }
+  }
+  return null;
+}
 
-  console.log(fontSize.value);
-  console.log(colorInput.value);
-  body.style.backgroundColor = `${colorInput.value}`;
-  body.style.fontSize = `${fontSize.value}px`
-})
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const fontSizeInput = document.getElementById('fontsize');
+  const fontColorInput = document.getElementById('fontcolor');
+
+  const fontSize = fontSizeInput.value;
+  const fontColor = fontColorInput.value;
+
+  setCookie('fontSize', fontSize, 30);
+  setCookie('fontColor', fontColor, 30);
+
+  applyUserPreferences();
+}
+
+function applyUserPreferences() {
+  const fontSize = getCookie('fontSize');
+  const fontColor = getCookie('fontColor');
+
+  if (fontSize) {
+    document.body.style.fontSize = fontSize + 'px';
+  }
+  if (fontColor) {
+    document.body.style.color = fontColor;
+  }
+}
+
+const preferencesForm = document.getElementById('preferences-form');
+preferencesForm.addEventListener('submit', handleFormSubmit);
+
+applyUserPreferences();
