e7779418b75c2defea84b382ae9559b85f47b51a
Bhuminandan <134225833+Bhuminandan@users.noreply.github.com>
2023-08-21 16:05:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+let body = document.getElementsByTagName("body")[0];
+let form = document.getElementsByTagName("form")[0];
+let submitBtn = document.querySelector("#sumbit-btn");
+
+submitBtn.addEventListener("click", (element) => {
+  element.preventDefault();
+  let fontSize = document.querySelector("#fontsize");
+  let colorInput = document.querySelector("#fontcolor");
+
+  console.log(fontSize.value);
+  console.log(colorInput.value);
+  body.style.backgroundColor = `${colorInput.value}`;
+  body.style.fontSize = `${fontSize.value}px`
+})
